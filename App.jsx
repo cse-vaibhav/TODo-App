5f81eacb248ee6c501eaceccc7ff0005d7c35ce2
cse-vaibhav <jainvaibhav671@gmail.com>
2023-04-28 19:08:27 +0530

    Registered on Unstop

---
+++ App.jsx
@@ -0,0 +1,40 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import HomePage from './pages/HomePage.jsx';
+import StartPage from './pages/StartPage.jsx';
+import LoginPage from './pages/LoginPage.jsx';
+import SignUpPage from './pages/SignUpPage.jsx';
+
+const App = () => {
+    const Stack = createNativeStackNavigator();
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen 
+                    name="Start"
+                    component={StartPage}
+                    options={{title: 'Welcome', headerShown: false}}
+                />
+                <Stack.Screen 
+                    name="Home"
+                    component={HomePage}
+                    options={{title: 'Tasks', headerShown: false}}
+                />
+                <Stack.Screen 
+                    name="Login"
+                    component={LoginPage}
+                    options={{title: 'LogIn', headerShown: false}}
+                />
+                <Stack.Screen 
+                    name="Register"
+                    component={SignUpPage}
+                    options={{title: 'Register', headerShown: false}}
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+};
+
+export default App;
